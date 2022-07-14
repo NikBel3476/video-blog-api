fbb419e99c474a2bb3cb8d37b31a47607f4691bb
NikBel3476 <nikita.belkin3476@gmail.com>
2022-07-14 22:23:00 +0400

    feature(arch): add Infarstructure.Data project

---
+++ Infrastructure.Data/UserContext.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+	public class UserContext : DbContext
+	{
+		public DbSet<User> Users => Set<User>();
+	}
+}
