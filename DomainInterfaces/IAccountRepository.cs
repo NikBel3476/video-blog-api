fc31284d078b116a4f1c7659e549e35944254751
NikBel3476 <nikita.belkin3476@gmail.com>
2022-07-14 21:08:49 +0400

    feature(arch): add Domain.Interfaces repository project

---
+++ DomainInterfaces/IAccountRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Interfaces
+{
+	public interface IAccountRepository
+	{
+		Account Get(long id);
+		void Create(Account account);
+		void Update(Account account);
+		void Delete(Account account);
+	}
+}
