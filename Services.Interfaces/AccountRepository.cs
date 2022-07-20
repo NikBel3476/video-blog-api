ff3356dfe988f7a5f253e52f63846ae14257c52c
NikBel3476 <nikita.belkin3476@gmail.com>
2022-07-21 00:00:04 +0400

    feature(services): add accountRepository

---
+++ Services.Interfaces/AccountRepository.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Core.Entities;
+using Domain.Interfaces;
+
+namespace Services.Interfaces
+{
+	public class AccountRepository : IAccountRepository
+	{
+		public void Create(Account account)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Delete(Account account)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update(Account account)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
