3ecb283d6d45ad04155c54935b3922b578ad9234
NikBel3476 <nikita.belkin3476@gmail.com>
2022-08-31 23:51:32 +0400

    feature: add api exception

---
+++ Exceptions/ApiException.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace Exceptions;
+
+public class ApiException : Exception
+{
+	public ApiException(HttpStatusCode statusCode) : base() { }
+
+	public ApiException(HttpStatusCode statusCode, string message) : base(message) { }
+}
